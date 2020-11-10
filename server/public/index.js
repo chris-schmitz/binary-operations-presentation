b6b29cb6da7d2998aad7e12fd6d123a0e6d24c08
chris-schmitz <schmitz.chris@gmail.com>
2020-11-09 22:08:28 -0600

    roughs in player

--- server/public/index.js
+++ server/public/index.js
@@ -7,7 +7,7 @@
 // *    - UI manager (handles the html)
 // *    - State manager (handles the html)
 // * - Game class takes ui and state manager instances as dependencies
-class BrickMover {
+class GridManager {
   gridElement = null
   RandomBrickToggleButton = null
   randomBricksOn = false
@@ -19,11 +19,21 @@ class BrickMover {
   animationIntervalContainer = null
   randomBrickIntervalContainer = null
 
+  player = {
+    current: {
+      row: 0,
+      column: 0,
+    },
+    previous: {
+      row: 0,
+      column: 0,
+    },
+  }
+
   constructor(rows, columns) {
     this.rows = rows
     this.columns = columns
-    for (let i = 0; i < rows; i++)
-    {
+    for (let i = 0; i < rows; i++) {
       this.gridState.push(0)
     }
   }
@@ -32,18 +42,16 @@ class BrickMover {
     this.grabElements()
     this.addHooks()
     this.populateGrid()
+    this.paintPlayer()
     this.startAnimation()
   }
 
   startAnimation() {
-    if (this.animationIntervalContainer)
-    {
+    if (this.animationIntervalContainer) {
       // * if the interval is already running, no need to start it again
       return
     }
-    this.animationIntervalContainer = setInterval(
-      this.animate.bind(this),
-      this.frameInterval)
+    this.animationIntervalContainer = setInterval(this.animate.bind(this), this.frameInterval)
   }
 
   stopAnimation() {
@@ -52,8 +60,7 @@ class BrickMover {
   }
 
   startRandomBricks() {
-    if (this.randomBrickIntervalContainer)
-    {
+    if (this.randomBrickIntervalContainer) {
       // * if the interval is already running, no need to start it again
       return
     }
@@ -75,27 +82,25 @@ class BrickMover {
   }
 
   addHooks() {
-    this.RandomBrickToggleButton.addEventListener("click", () => {
-      if (this.randomBricksOn)
-      {
+    document.addEventListener('keydown', this.movePlayer.bind(this))
+
+    this.RandomBrickToggleButton.addEventListener('click', () => {
+      if (this.randomBricksOn) {
         this.randomBricksOn = false
-        this.RandomBrickToggleButton.classList.remove("button-toggle-on")
+        this.RandomBrickToggleButton.classList.remove('button-toggle-on')
         this.stopRandomBricks()
-      } else
-      {
+      } else {
         this.randomBricksOn = true
-        this.RandomBrickToggleButton.classList.add("button-toggle-on")
+        this.RandomBrickToggleButton.classList.add('button-toggle-on')
         this.startRandomBricks()
       }
     })
   }
 
   populateGrid() {
-    for (let rowIterator = 0; rowIterator < this.rows; rowIterator++)
-    {
+    for (let rowIterator = 0; rowIterator < this.rows; rowIterator++) {
       const row = document.createElement('tr')
-      for (let columnIterator = 0; columnIterator < this.columns; columnIterator++)
-      {
+      for (let columnIterator = 0; columnIterator < this.columns; columnIterator++) {
         const cell = document.createElement('td')
         cell.setAttribute('column', columnIterator)
         row.appendChild(cell)
@@ -103,7 +108,7 @@ class BrickMover {
       this.gridElement.appendChild(row)
 
       const button = document.createElement('button')
-      button.addEventListener('click', () => brickMover.addBrick(rowIterator))
+      button.addEventListener('click', () => gridManager.addBrick(rowIterator))
       button.innerText = `Row: ${rowIterator + 1}`
       this.buttonWrapper.appendChild(button)
     }
@@ -118,16 +123,42 @@ class BrickMover {
     this.paintCell(rowIndex, 0, true)
   }
 
+  movePlayer(event) {
+    this.player.previous.row = this.player.current.row
+    this.player.previous.column = this.player.current.column
+
+    switch (event.code) {
+      case 'ArrowUp':
+        this.player.current.row--
+        break
+      case 'ArrowDown':
+        this.player.current.row++
+        break
+      case 'ArrowLeft':
+        this.player.current.column--
+        break
+      case 'ArrowRight':
+        this.player.current.column++
+        break
+    }
+
+    this.paintPlayer()
+  }
+
+  // TODO: merge with paintCell
+  paintPlayer() {
+    this.gridElement.children[this.player.previous.row].children[this.player.previous.column].removeAttribute('paint-player')
+    this.gridElement.children[this.player.current.row].children[this.player.current.column].setAttribute('paint-player', 'true')
+  }
+
   paintCell(row, column, state) {
     this.gridElement.children[row].children[column].setAttribute('paint', state)
   }
 
   paintFrame() {
-    for (let rowIterator = 0; rowIterator < this.gridState.length; rowIterator++)
-    {
+    for (let rowIterator = 0; rowIterator < this.gridState.length; rowIterator++) {
       const row = this.gridElement.children[rowIterator]
-      for (let columnIterator = 0; columnIterator < this.columns; columnIterator++)
-      {
+      for (let columnIterator = 0; columnIterator < this.columns; columnIterator++) {
         const cellState = Math.pow(2, columnIterator) & this.gridState[rowIterator]
         this.paintCell(rowIterator, columnIterator, cellState !== 0)
       }
@@ -135,8 +166,7 @@ class BrickMover {
   }
 
   animate() {
-    for (let i = 0; i < this.gridState.length; i++)
-    {
+    for (let i = 0; i < this.gridState.length; i++) {
       this.gridState[i] = this.gridState[i] << 1
       // Mask off the numbers so theyt don't just continually grow once the bricks are "off" the grid
       this.gridState[i] = this.gridState[i] & (Math.pow(2, this.columns) - 1)
@@ -145,12 +175,11 @@ class BrickMover {
   }
 }
 
-const brickMover = new BrickMover(8, 10)
+const gridManager = new GridManager(8, 10)
 
 document.addEventListener('readystatechange', () => {
-  if (document.readyState === 'complete')
-  {
-    brickMover.begin()
-    window.game = brickMover // ! leaving in for debugging and demo purposes
+  if (document.readyState === 'complete') {
+    gridManager.begin()
+    window.gridManager = gridManager // ! leaving in for debugging and demo purposes
   }
 })
