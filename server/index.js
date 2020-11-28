dc1298e6594c93408e309a0a2c5fc8c577701500
Chris Schmitz <schmitz.chris@gmail.com>
2020-11-28 08:57:44 -0600

    Magic bullet: messaging from touch controller to UI

--- server/index.js
+++ server/index.js
@@ -1,24 +1,20 @@
 const http = require('http')
-
 const express = require('express')
-
 const WebSocket = require('ws')
 
 const PORT = 3001
-
 const app = express()
-
 app.use(express.static('public'))
-
 const server = http.createServer(app)
-
 const websocketServer = new WebSocket.Server({ server })
 
 websocketServer.on('connection', (socket) => {
   console.log(websocketServer.clients)
   socket.on('message', (message) => {
+    const intVersion = message.readUInt32LE()
+    console.log(intVersion.toString(2))
     websocketServer.clients.forEach((client) => {
-      client.send(message)
+      client.send(intVersion)
     })
   })
 })
