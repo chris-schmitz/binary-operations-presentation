f92b9073b3bd99e44bebb0aa25129f9fe74c4f60
Chris Schmitz <schmitz.chris@gmail.com>
2020-12-05 21:03:44 -0600

    Annotates all methods and functions

--- server/public/game-board.js
+++ server/public/game-board.js
@@ -53,16 +53,25 @@ class GridManager {
     this.player.health = this.startingHealth = playerHealth
 
     this.setKeyStateOrder()
-    this.initializeGridState()
+    this.resetGridState()
     this.initializePlayerState()
   }
-  initializeGridState() {
+
+  /**
+   * Sets or resets the number representation of the grid and blocks within.
+   *
+   * We want to start with no-bricks, so the state for everything should be 0.
+   */
+  resetGridState() {
     this.gridState = []
     for (let i = 0; i < this.rows; i++) {
       this.gridState.push(0)
     }
   }
 
+  /**
+   * Sets the starting position of the player based on the size of the grid.
+   */
   initializePlayerState() {
     // * Start the player character half way through the rows
     this.player.rowIndex = Math.floor(this.rows / 2)
@@ -71,67 +80,9 @@ class GridManager {
     this.player.columnState = 0b1
   }
 
-  begin() {
-    this.grabElements()
-    this.setGameState('Playing')
-    this.addHooks()
-    this.populateGrid()
-    this.paintPlayer()
-    this.startAnimation()
-  }
-
-  setGameState(label) {
-    this.gameState.innerText = label
-  }
-
-  setKeyStateOrder() {
-    this.keyStates = [this.KEY_LEFT, this.KEY_UP, this.KEY_RIGHT, this.KEY_DOWN]
-  }
-
-  startAnimation() {
-    if (this.animationIntervalContainer) {
-      // * if the interval is already running, no need to start it again
-      return
-    }
-    this.animationIntervalContainer = setInterval(this.animate.bind(this), this.frameInterval)
-  }
-
-  setFrameRate(rate) {
-    this.frameInterval = rate
-    this.stopAnimation()
-    this.startAnimation()
-
-    if (this.randomBrickIntervalContainer !== null) {
-      this.stopRandomBricks()
-      this.startRandomBricks()
-    }
-  }
-
-  stopAnimation() {
-    clearInterval(this.animationIntervalContainer)
-    this.animationIntervalContainer = null
-  }
-
-  startRandomBricks() {
-    if (this.randomBrickIntervalContainer) {
-      // * if the interval is already running, no need to start it again
-      return
-    }
-    this.randomBricksOn = true
-    this.randomBrickToggleButton.classList.add('button-toggle-on')
-    this.randomBrickIntervalContainer = setInterval(() => {
-      const randomIndex = Math.floor(Math.random() * this.gridState.length)
-      this.addBrick(randomIndex)
-    }, this.frameInterval / 3)
-  }
-
-  stopRandomBricks() {
-    clearInterval(this.randomBrickIntervalContainer)
-    this.randomBricksOn = false
-    this.randomBrickToggleButton.classList.remove('button-toggle-on')
-    this.randomBrickIntervalContainer = null
-  }
-
+  /**
+   * Stores references to all of the HTML elements used in the UI.
+   */
   grabElements() {
     this.gridElement = document.querySelector('tbody')
     this.buttonWrapper = document.querySelector('#button-wrapper')
@@ -148,6 +99,9 @@ class GridManager {
     this.collisionIndicator = document.querySelector('.collision-indicator')
   }
 
+  /**
+   * Adds event listeners to various html elements.
+   */
   addHooks() {
     document.addEventListener('keydown', this.playerKeydownHandler.bind(this))
     document.addEventListener('keyup', this.playerKeyupHandler.bind(this))
@@ -163,6 +117,106 @@ class GridManager {
     })
   }
 
+  /**
+   * Kicks off the game setup once the dom is ready.
+   *
+   * Note that this is different than the concept of reseting an already running game.
+   */
+  begin() {
+    this.grabElements()
+    this.setGameState('Playing')
+    this.addHooks()
+    this.populateGrid()
+    this.paintPlayer()
+    this.startBrickAnimation()
+  }
+
+  /**
+   * Sets the dom element that shows the State of the game.
+   * e.g. "Playing" vs "Game Over"
+   *
+   * @param {string} label The text you want to set the game state to.
+   */
+  setGameState(label) {
+    this.gameState.innerText = label
+  }
+
+  /**
+   * Sets the order of the keys that we're listening for.
+   *
+   * Note that this is only really needed because of how we're handling the key listeners with branchless code.
+   */
+  setKeyStateOrder() {
+    this.keyStates = [this.KEY_LEFT, this.KEY_UP, this.KEY_RIGHT, this.KEY_DOWN]
+  }
+
+  /**
+   * Starts the animation interval that drives the bricks.
+   */
+  startBrickAnimation() {
+    if (this.animationIntervalContainer) {
+      // * if the interval is already running, no need to start it again
+      return
+    }
+    this.animationIntervalContainer = setInterval(this.animateBrickState.bind(this), this.frameInterval)
+  }
+
+  /**
+   * The interval that the animation should run at. I.e. Frames per second (kind of).
+   * @param {Number} rate The number in milliseconds
+   */
+  setBrickAnimationFrameRate(rate) {
+    this.frameInterval = rate
+    this.stopBrickAnimation()
+    this.startBrickAnimation()
+
+    if (this.randomBrickIntervalContainer !== null) {
+      this.stopRandomBricks()
+      this.startRandomBricks()
+    }
+  }
+
+  /**
+   * Clears the animation interval which stops the bricks.
+   */
+  stopBrickAnimation() {
+    clearInterval(this.animationIntervalContainer)
+    this.animationIntervalContainer = null
+  }
+
+  /**
+   * Starts the automatic generation of bricks in random rows.
+   */
+  startRandomBricks() {
+    if (this.randomBrickIntervalContainer) {
+      // * if the interval is already running, no need to start it again
+      return
+    }
+    this.randomBricksOn = true
+    this.randomBrickToggleButton.classList.add('button-toggle-on')
+
+    const bricksPerFrame = 3
+    this.randomBrickIntervalContainer = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * this.gridState.length)
+      this.addBrick(randomIndex)
+    }, this.frameInterval / bricksPerFrame)
+  }
+
+  /**
+   * Stops the automatic generation of random bricks.
+   */
+  stopRandomBricks() {
+    clearInterval(this.randomBrickIntervalContainer)
+    this.randomBricksOn = false
+    this.randomBrickToggleButton.classList.remove('button-toggle-on')
+    this.randomBrickIntervalContainer = null
+  }
+
+  /**
+   * Builds UI for the grid based on the row and grid count specified.
+   *
+   * TODO: come back and extract pieces needed to make the grid size updatable
+   */
   populateGrid() {
     for (let rowIterator = 0; rowIterator < this.rows; rowIterator++) {
       const row = document.createElement('tr')
@@ -180,17 +234,27 @@ class GridManager {
     }
   }
 
+  /**
+   * Adds a single brick to a row that will be picked up by the next frame render.
+   *
+   * @param {Number} rowIndex The row number to add a single brick to.
+   */
   addBrick(rowIndex) {
     // * Note that we're not adding 1 to the beginning of our state, we're performing a bitwise `OR` on it.
     // * So, regardless of the state of the first bit of our state, we're setting it to `1`,
     // * This way we don't accidentally push more bricks into our row if we accidentally multi-click the button
     // * before the next frame paint. You can think of this as a way of debouncing the button until the next frame.
     this.gridState[rowIndex] = this.gridState[rowIndex] | Math.pow(2, this.columns - 1)
-    // this.gridState[rowIndex] = this.gridState[rowIndex] | 1
-    this.paintCell(rowIndex, 0, true)
+    this.paintBrick(rowIndex, 0, true)
   }
 
-  // TODO: abstract some logic, there's a lot happening here that should be named
+  /**
+   * Logic for moving the player when the player hits one of the arrow keys.
+   *
+   * Note that this allows for multi-press support.
+   *
+   * @param {keydown} event The keydown event from the keyboard
+   */
   playerKeydownHandler(event) {
     if (!['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(event.code)) return
     if (!this.enableArrowKeys) return
@@ -229,6 +293,12 @@ class GridManager {
     this.detectCollisions()
   }
 
+  /**
+   * Updates the player position to note a key being lifted.
+   *
+   *
+   * @param {keyup} event The keyup event from the keyboard.
+   */
   playerKeyupHandler(event) {
     // * leaving the early exit in with the string names for a more readable conditional
     if (!['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(event.code)) return
@@ -258,6 +328,9 @@ class GridManager {
     this.updateActiveKeyDisplay()
   }
 
+  /**
+   * Decrementing the player health if we're tracking collisions between the player and the bricks.
+   */
   decrementPlayerHealth() {
     if (!this.decrementHealthOnCollision) return
 
@@ -267,11 +340,16 @@ class GridManager {
     }
   }
 
+  /**
+   * Removes the player visually from the grid.
+   */
   clearPlayer() {
     this.gridElement.querySelector('[paint-player]')?.removeAttribute('paint-player')
   }
 
-  // TODO: merge with paintCell
+  /**
+   * Paints the player on the grid based on the current player position state
+   */
   paintPlayer() {
     // ^ It's so funny, I was running through different ways of efficiently clear the old player position visually. None of the
     // ^ bitwise operation approaches seemed to be a good option because the paint timing would be tricky with the brick paint timing.
@@ -285,21 +363,35 @@ class GridManager {
     )
   }
 
-  paintCell(row, column, state) {
+  /**
+   * Painting the brick onto the grid UI.
+   *
+   * @param {Number} row The row to paint the brick
+   * @param {Number} column The column to paint the brick
+   * @param {Boolean} state How to paint the brick, either on or off
+   */
+  paintBrick(row, column, state) {
     this.gridElement.children[row].children[column].setAttribute('paint', state)
   }
 
-  // TODO cleanup
+  /**
+   * Paints all of the bricks for the current frame given the grid state.
+   */
   paintFrame() {
     for (let rowIterator = 0; rowIterator < this.gridState.length; rowIterator++) {
       for (let columnIterator = 0; columnIterator < this.columns; columnIterator++) {
         const cellState = Math.pow(2, columnIterator) & this.gridState[rowIterator]
-        this.paintCell(rowIterator, this.columns - columnIterator - 1, cellState !== 0)
+        this.paintBrick(rowIterator, this.columns - columnIterator - 1, cellState !== 0)
       }
     }
   }
 
-  animate() {
+  /**
+   * Animates brick STATE across the grid based on the grid state.
+   *
+   * Note that this is decoupled from actually painting the bricks onto the grid
+   */
+  animateBrickState() {
     for (let i = 0; i < this.gridState.length; i++) {
       this.gridState[i] = this.gridState[i] >> 1
       // this.gridState[i] = this.gridState[i] << 1
@@ -311,11 +403,9 @@ class GridManager {
     this.paintFrame()
   }
 
-  updateStateOutput() {
-    this.gridStateOutput.innerText = this.gridState
-    this.playerStateOutput.innerText = JSON.stringify(this.player)
-  }
-
+  /**
+   * Determines if the player and brick occupy the same cell in the grid.
+   */
   // TODO: come back and annotate and cleanup
   detectCollisions() {
     const colissions = this.player.columnState & this.gridState[this.player.rowIndex]
@@ -343,6 +433,21 @@ class GridManager {
     this.updateStateOutput()
   }
 
+  /**
+   * **UTILITY METHOD**
+   *
+   * Draw the various states to the dom. Not needed for the game, just helpful for the demo and debugging.
+   */
+  updateStateOutput() {
+    this.gridStateOutput.innerText = this.gridState
+    this.playerStateOutput.innerText = JSON.stringify(this.player)
+  }
+
+  /**
+   * **UTILITY METHOD**
+   *
+   * Draws the active arrow key to the screen. Not needed for the game, just another helpful visual.
+   */
   updateActiveKeyDisplay() {
     // TODO: move to global spot, use to back enums
     this.keyIndicators.up.setAttribute('key-active', (this.activeKeys & 0b1000) > 0)
@@ -351,6 +456,9 @@ class GridManager {
     this.keyIndicators.right.setAttribute('key-active', (this.activeKeys & 0b0001) > 0)
   }
 
+  /**
+   * Resets the game states, the grid UI, and starts the game play over.
+   */
   restartGame() {
     this.player.health = this.startingHealth
     this.enableArrowKeys = true
@@ -360,24 +468,37 @@ class GridManager {
     this.clearAllBricks()
     this.initializePlayerState()
     this.paintPlayer()
-    this.initializeGridState()
+    this.resetGridState()
     this.startRandomBricks()
-    this.startAnimation()
+    this.startBrickAnimation()
   }
 
+  /**
+   * Stops the gameplay, locks the arrow keys, and updates the UI to show that the game has ended.
+   */
   triggerGameOver() {
     clearInterval(this.animationIntervalContainer)
     this.stopRandomBricks()
     this.setGameState('Game Over')
     this.showGameOverGraphics()
-    this.stopAnimation()
+    this.stopBrickAnimation()
     this.enableArrowKeys = false
   }
 
+  /**
+   * Removes all bricks from the grid UI.
+   *
+   * **Note** that this is not the same as resetting the brick state (though it prob should be linked :|)
+   */
   clearAllBricks() {
     document.querySelectorAll('[paint = true]').forEach((cell) => cell.setAttribute('paint', false))
   }
 
+  /**
+   * Updates the UI with to either show or hide the game over visual elements.
+   *
+   * @param {Boolean} status Whether or not to show the game over graphics
+   */
   showGameOverGraphics(status = true) {
     if (status) {
       this.gridElement.classList.add('game-over')
@@ -391,26 +512,28 @@ class GridManager {
 
 const gridManager = new GridManager(12, 10, 10)
 
-// TODO: come back and clean this up. either add it as a new class or fold it into the eexisting class
+/**
+ * * All of the websocket stuff
+ * * I'm keeping all of this out of the grid manager for a couple of reasons:
+ * ? - The grid manager is already waaay way to big and should def be broken out into subclasses
+ * ? - In the final version of this code the grid manager is going to get broken up and the state management
+ * ?    will be moved up to the server. Knowing that I want to keep the Communication stuff separate.
+ */
 
 const socket = new WebSocket('ws://localhost:3001')
-socket.addEventListener('open', () => {
-  console.log('connected to socket server')
-})
+socket.addEventListener('open', () => console.log('connected to socket server'))
 socket.addEventListener('close', () => console.log('disconnected from socket server'))
 socket.addEventListener('error', (error) => {
   console.log('Websocket server error:')
   console.log(error)
 })
 
-// TODO: refactor to allow this to accept the message `type` and the message `data`
-async function touchControllerMessageHandler(type, data) {
-  // TODO I dont' like how this feels. refactor and pull out to it's own function or method
-  // switch (type) {
-  //   case messageTypeMockEnum['add-single-brick']:
-  //     gridManager.addBrick(data)
-  //     break
-  // case messageTypeMockEnum['touch-controller-state']:
+/**
+ * Takes in the binary state of the touch controller and creates bricks in the first 12 rows based on that state.
+ *
+ * @param {Number} data The 12 bit state of the touch controller
+ */
+async function touchControllerMessageHandler(data) {
   let maskBit = 0b1
   for (let i = 0; i < 12; i++) {
     let maskedData = data & maskBit
@@ -424,16 +547,26 @@ async function touchControllerMessageHandler(type, data) {
     }
     maskBit <<= 1
   }
-  //     break
-  // }
 }
 
-const messageTypeMockEnum = {
-  'add-single-brick': 0b00000001,
-  'touch-controller-state': 0b00000010,
+/**
+ * The web controllers are tied to one specific row. This handler adds a brick for the current frame to
+ * the controllers assigned row.
+ *
+ * @param {Number} rowIndex The row that a brick should be added to
+ */
+function webControllerMessageHandler(rowIndex) {
+  gridManager.addBrick(rowIndex)
 }
+
 const messageTypeMask = 0xff
 const touchStateMask = 0xfff
+
+/**
+ * Pulls the message type and data out of the numeric message from the touch controller.
+ *
+ * @param {Number} message The message data and type combined into a single binary string.
+ */
 function parseMessageTypeAndData(message) {
   const type = message & messageTypeMask
   message >>= 8
@@ -441,49 +574,39 @@ function parseMessageTypeAndData(message) {
   return { type, data }
 }
 
-function byteArrayMessageHandler(byteArray) {
-  console.log(byteArray)
-  const type = byteArray[0]
-  const message = byteArray[1]
-
-  switch (type) {
-    case 0b1:
-      console.log('Message from web controller')
-      gridManager.addBrick(message)
-      break
-    case 0b10:
-      console.log('Message from touch controller')
-      touchControllerMessageHandler(type, message)
-      break
-  }
-}
-
-// TODO: woof, refactor to clean this up. We need:
-// * - a "parseMessage(message): {type, data}" function:
-// ? - check the instance
-// ? - parse for number
-// ? - convert for blob
-// ? - extracts and returns type and data regardless of instance
-// * - hand off type and data to a single `messageHandler` function
+/**
+ * Handler for any messages coming from the websocket server.
+ */
 socket.addEventListener('message', async (message) => {
-  const { data } = message
+  const { data: payload } = message
 
-  if (data instanceof Number) {
-    const number = Number(data)
+  let type = null
+  let data = null
 
-    const { type, data } = parseMessageTypeAndData(number)
-    touchControllerMessageHandler(type, data)
-  } else if (data instanceof Blob) {
-    const buffer = await data.arrayBuffer()
-    const view = new Uint8Array(buffer)
-
-    byteArrayMessageHandler(view)
+  if (payload instanceof Number) {
+    const number = Number(payload)
+    ;({ type, data } = parseMessageTypeAndData(number))
+  } else if (payload instanceof Blob) {
+    const buffer = await payload.arrayBuffer()
+    ;[type, data] = new Uint8Array(buffer)
   } else {
     console.error("we don't have a handler for the message.")
     console.log(message)
   }
+
+  switch (type) {
+    case 0b01:
+      webControllerMessageHandler(data)
+      break
+    case 0b10:
+      touchControllerMessageHandler(data)
+      break
+  }
 })
 
+/**
+ * Waiting for the dom to be ready before kicking everything off.
+ */
 document.addEventListener('readystatechange', () => {
   if (document.readyState === 'complete') {
     gridManager.begin()
